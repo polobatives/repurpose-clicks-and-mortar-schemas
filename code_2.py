dca34778f4abf4e283d7fca4ba3e0a4334865e89
polobatives <143891829+polobatives@users.noreply.github.com>
2023-12-03 08:59:26 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,14 @@
+def generate_random_data():
+    main()
+    return data
+
+        print(f"Random Number: {item}")
+
+
+
+if __name__ == "__main__":
+    for item in data:
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+    data = generate_random_data()
